45baf621020c527caa5c4df28814f2a37b3848a0
Ervinne Sodusta <ervinne.sodusta@nuworks.ph>
2022-06-24 17:02:40 +0800

    Fixup vue-bootstrap issue on modal overlays

--- laravel_backend/resources/js/pages/RolesPage.vue
+++ laravel_backend/resources/js/pages/RolesPage.vue
@@ -1,28 +1,67 @@
 <template>
-  <div class="container">
-    <h1>Roles</h1>
-
+  <div>
     <div>
-      <b-table striped hover :items="roles"></b-table>
+      <h1>Roles</h1>
 
-      <b-button v-b-modal.modal-form right variant="success">Add Role</b-button>
-    </div>
-
-    <b-modal id="modal-form" title="BootstrapVue">
       <div>
+        <b-table striped hover :items="roles"></b-table>
+
+        <b-button v-b-modal.modal-role-form>Add Role</b-button>
+      </div>
+    </div>
+    <b-modal
+      id="modal-role-form"
+      ref="modal"
+      v-bind:title="form.title"
+      v-bind:ok-title="form.action"
+    >
+      <b-form @submit.stop.prevent="onSubmit" @reset="onReset">
+        <!-- <b-button class="mt-3" block @click="$bvModal.hide('modal-form')"
+          >Cancel</b-button
+        >
+        <b-button class="mt-3" block type="submit" variant="primary">{{
+          form.action
+        }}</b-button> -->
+      </b-form>
+
+      <b-container>
         <b-row>
           <b-col> Display Name </b-col>
-          <b-col>
-            <b-form-input v-model="form.name" placeholder="Role Code"></b-form-input>
+          <b-col sm="8">
+            <b-form-group
+              label-for="in-role-name"
+              v-bind:description="form.error.name"
+              class="mb-0 text-danger"
+            >
+              <b-form-input
+                id="in-role-name"
+                v-model="form.name"
+                placeholder="Auditor/User/etc"
+              ></b-form-input>
+            </b-form-group>
           </b-col>
         </b-row>
 
-        <b-form-input v-model="form.name" placeholder="Role Name"></b-form-input>
-        <div class="mt-2">Value: {{ form.code }}</div>
-      </div>
+        <b-row>
+          <b-col> Description </b-col>
+          <b-col sm="8">
+            <b-form-input
+              v-model="form.description"
+              placeholder="What the role will be used for"
+            ></b-form-input>
+          </b-col>
+        </b-row>
+      </b-container>
     </b-modal>
   </div>
 </template>
+
+<style>
+/* Work-around for that weird modal overlay with excessively high z-index */
+#modal-role-form {
+  z-index: 1060;
+}
+</style>
 
 <script>
 export default {
@@ -35,6 +74,12 @@ export default {
       form: {
         code: "",
         name: "",
+        title: "Create new Role",
+        action: "Save",
+        error: {
+          name: "Role name already taken",
+          description: "",
+        },
       },
     };
   },
@@ -42,6 +87,9 @@ export default {
     const roles = (await axios.get("/api/roles")).data;
     console.log(roles);
     this.roles = roles;
+  },
+  async onSubmit() {
+    console.log(this.form);
   },
 };
 </script>
